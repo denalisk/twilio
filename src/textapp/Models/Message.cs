5d79cabfebaf70bf1f28be9c85e954755bf5b0fb
Sam Kirsch <samdkirsch@gmail.com>
2017-05-09 16:49:07 -0700

    make a basic texting app

---
+++ src/textapp/Models/Message.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace textapp.Models
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+
+        public static List<Message> GetMessages()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/AC5c28af34fb8f233b541f509f69f58a7f/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("AC5c28af34fb8f233b541f509f69f58a7f", "5a35ce1fc7079ddd1ece2bf499bb21b5");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            return messageList;
+        }
+
+        public void Send()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/AC5c28af34fb8f233b541f509f69f58a7f/Messages.json", Method.POST);
+            request.AddParameter("To", To);
+            request.AddParameter("From", From);
+            request.AddParameter("Body", Body);
+            client.Authenticator = new HttpBasicAuthenticator("AC5c28af34fb8f233b541f509f69f58a7f", "5a35ce1fc7079ddd1ece2bf499bb21b5");
+            client.ExecuteAsync(request, response => { Console.WriteLine(response.Content); });
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
---
+++ src/textapp/Models/textappDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace textapp.Models
+{
+    public class textappDbContext : DbContext
+    {
+        public textappDbContext()
+        {
+
+        }
+        public textappDbContext(DbContextOptions<textappDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=textappDb;integrated security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        
+    }
+}
